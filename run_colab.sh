4c0038f5d270270c6da3a7812f12a1f124ae5ff3
rachung2510 <chungrc.rachel@gmail.com>
2022-02-20 07:22:06 +0800

    update run_colab.sh

--- run_colab.sh
+++ run_colab.sh
@@ -14,8 +14,8 @@ python joint_training.py \
     --pretrained_model_cfg bert-base-uncased \
     --model_file "/content/drive/My Drive/Colab Notebooks/RetGen/models/arxiv_retriever.pkl" \
     --ctx_file "/content/drive/My Drive/Colab Notebooks/RetGen/data/wiki.txt" \
-    --num_shards 1 \
-    --batch_size 16 \
+    --num_shards 1000 \
+    --batch_size 32 \
     --n_docs 2 \
     --encoding \
     --load_trained_model      
