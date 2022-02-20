4bb0e029d8976a70ff220676fdcc0e352dd12498
rachung2510 <chungrc.rachel@gmail.com>
2022-02-20 17:53:48 +0800

    decrease batch size

--- run_colab.sh
+++ run_colab.sh
@@ -15,7 +15,7 @@ python joint_training.py \
     --model_file "/content/drive/My Drive/Colab Notebooks/RetGen/models/arxiv_retriever.pkl" \
     --ctx_file "/content/drive/My Drive/Colab Notebooks/RetGen/data/wiki.txt" \
     --num_shards 1000 \
-    --batch_size 32 \
+    --batch_size 16 \
     --n_docs 2 \
     --encoding \
     --load_trained_model      
