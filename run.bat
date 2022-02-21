34ca46f992e48b0ce018808f0b9e59c210df9355
rachung2510 <chungrc.rachel@gmail.com>
2022-02-21 23:49:48 +0800

    decrease num_shard

--- run.bat
+++ run.bat
@@ -1,12 +1,12 @@
 :: model training
 ::CUDA_VISIBLE_DEVICES=0 
-python joint_training.py ^
+python test.py ^
     --model_name_or_path configs ^
     --init_checkpoint models/arxiv_generator.pkl ^
     --train_input_file data/arxiv_train.512len.db ^
     --eval_input_file data/arxiv_test.txt ^
     --output_dir outputs/joint_arxiv ^
-    --file_suffix joint_wiki ^
+    --file_suffix joint_arxiv ^
     --train_batch_size 1 ^
     --gradient_accumulation_steps 1 ^
     --eval_batch_size 1 ^
@@ -15,7 +15,7 @@ python joint_training.py ^
     --pretrained_model_cfg bert-base-uncased ^
     --model_file models/arxiv_retriever.pkl ^
     --ctx_file data/wiki.txt ^
-    --num_shards 1000 ^
+    --num_shards 10000 ^
     --batch_size 8 ^
     --n_docs 2 ^
     --encoding ^
