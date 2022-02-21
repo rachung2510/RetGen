061bc6037a48398391769a294ed8fe7b82cfc997
rachung2510 <chungrc.rachel@gmail.com>
2022-02-21 10:08:26 +0800

    update retriever path

--- run_colab.sh
+++ run_colab.sh
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=0 python joint_training.py \
     --num_optim_steps 16000 \
     --encoder_model_type ance_roberta \
     --pretrained_model_cfg bert-base-uncased \
-    --model_file "/content/drive/My Drive/Colab Notebooks/RetGen/models/retriever-pretrain-step-400.pkl" \
+    --model_file "/content/drive/My Drive/Colab Notebooks/RetGen/models/arxiv_retriever.pkl" \
     --ctx_file "/content/drive/My Drive/Colab Notebooks/RetGen/data/wiki.txt" \
     --num_shards 100 \
     --batch_size 8 \
